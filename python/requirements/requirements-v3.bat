9ab9a3267b8d1878f4f97e3f8b785dcc90d6e4c2
zaina3-dev <tzl@hotmail.fr>
2021-05-03 16:12:57 +0200

    maj requirements and unitest

--- python/requirements/requirements-v3.bat
+++ python/requirements/requirements-v3.bat
@@ -1,24 +1,25 @@
 @ECHO OFF 
-TITLE Test programme parameters (python)
+TITLE TrafficRennes_TransfertData : install requirements (python)
+
 
 rem Variables
 SET python_exe=python
 rem C:\Users\formation\AppData\Local\Programs\Python\Python39\python.exe
-SET python_script=unitest.py
+SET python_requirements=requirements.txt
 
 
 rem Print
 ECHO Infos python
 ECHO -------------------------
 ECHO Python exe: %python_exe%
-ECHO Python unitest script: %python_script%
+ECHO Python requirements file: %python_requirements%
 
 
-rem Run python
+rem Install requirements
 ECHO. & ECHO.
-ECHO Run tests
+ECHO Install requirements
 ECHO -------------------------
-"%python_exe%" "%python_script%" -v
+%python_exe% -m pip install -r "%python_requirements%"
 
 
 PAUSE
