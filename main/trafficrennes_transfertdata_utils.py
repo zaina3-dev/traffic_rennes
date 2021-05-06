1d9b726bb204d452a939a51b451aec1c7afb6ae9
zaina3-dev <tzl@hotmail.fr>
2021-05-06 17:11:43 +0200

    add livrables

--- main/trafficrennes_transfertdata_utils.py
+++ main/trafficrennes_transfertdata_utils.py
@@ -4,6 +4,43 @@ import json
 from elasticsearch import Elasticsearch
 
 
+def second_to_time(s, strftime_out=["j","h","m","s"]):
+    """
+    Convertir des secondes en jours, heures, minutes et secondes
+    
+    Parameters
+    ----------
+    s : int
+        nombre de secondes en entrée
+
+    strftime_out : list of string
+        le format en sortie
+    
+    Returns
+    -------
+    String with the converted seconds.
+    
+    Notes
+    -----
+    j: jours, h:heures, m:minutes, s:secondes
+    """
+
+    # correspondance entre secondes et j,h,m,s
+    x = {"j":(1*60*60)*24, "h":(1*60)*60, "m":(1)*60, "s":1}
+    
+    # filtre si strftime_out
+    strftime_out = [i for i in strftime_out if i in x.keys()]
+    x = {i:x[i] for i in strftime_out}
+    
+    y = {}
+    for i in x.keys():
+        y[i] = int(s/x[i])
+        s = s%x[i]
+
+    z = [str(y[i])+i for i in y.keys() if y[i]>0]
+    z = " ".join(z)
+    return z
+
 # test le port elasticsearch
 def connect_elasticsearch(host, port):
     es = None
